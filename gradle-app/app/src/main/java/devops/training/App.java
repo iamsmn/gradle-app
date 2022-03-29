a2e57ea525e9d0fe2094f2d0b385ebbcafafe12f
suman <info.suman6@gmail.com>
2022-03-29 17:49:46 +0000

    minor change

--- gradle-app/app/src/main/java/devops/training/App.java
+++ gradle-app/app/src/main/java/devops/training/App.java
@@ -5,7 +5,7 @@ package devops.training;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World! Suman ";
     }
 
     public static void main(String[] args) {
